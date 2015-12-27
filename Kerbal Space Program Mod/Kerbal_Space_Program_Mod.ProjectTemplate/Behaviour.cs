97d601fac447e6221e1e169488640a197e1d4482
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-12-26 18:46:25 -0600

    Set up project to have MonoBehaviour as the first file and to resolve KSP references based on the specified KSPRoot.

---
+++ Kerbal Space Program Mod/Kerbal_Space_Program_Mod.ProjectTemplate/Behaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace $safeprojectname$
+{
+    class Behaviour : MonoBehaviour
+    {
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+
+        void OnDestroy()
+        {
+
+        }
+    }
+}
