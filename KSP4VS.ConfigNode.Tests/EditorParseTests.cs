135a0ee865e7b75d77c6f5fb4a95e6956bdf976a
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-02-14 21:06:58 -0600

    Added more ConfigNode tests.

---
+++ KSP4VS.ConfigNode.Tests/EditorParseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KSP4VS.ConfigNode.Tests
+{
+    public class EditorParseTests
+    {
+        [Fact]
+        public void NameWithNumberWorksOnlyInEditor()
+        {
+            var editorParser = new NodeParser(true);
+            var strictParser = new NodeParser(false);
+            var testString = "name2 = value";
+            Assert.Throws<FormatException>(() => strictParser.Parse(testString));
+            editorParser.Parse(testString);
+        }
+    }
+}
--- KSP4VS.ConfigNode.Tests/StandardParseTests.cs
+++ KSP4VS.ConfigNode.Tests/StandardParseTests.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace KSP4VS.ConfigNode.Tests
 {
     public class StandardParseTests
     {
+        [Fact]
+        public void EmptyConfigNodeNoNewlineParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse("PART {}");
+        }
+
+        [Fact]
+        public void EmptyConfigNodeWithNewlineParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse("PART\r\n{ }");
+        }
+
+        [Fact]
+        public void ConfigNodeWithValueEntriesParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse(@"
+                NODE
+                {
+                name = value
+                }");
+        }
+
+        [Fact]
+        public void TopLevelValueEntryParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse("name = value");
+        }
+
+        [Fact]
+        public void ConfigNodeContainingConfigNodeParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse(@"
+                NODE
+                {
+                    SUBNODE
+                    {
+                        name = value
+                    }
+                }");
+        }
+
+        [Fact]
+        public void TwoTopLevelConfigNodesParses()
+        {
+            var parser = new NodeParser(false);
+            parser.Parse(@"
+                NODE {}
+                NODE {}");
+        }
     }
 }
