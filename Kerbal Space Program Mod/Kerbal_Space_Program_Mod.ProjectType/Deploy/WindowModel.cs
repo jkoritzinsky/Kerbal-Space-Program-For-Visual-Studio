1f63b3f40ece4ae6bddbcacead28865a7d53f6ed
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-02-23 22:36:07 -0600

    Somehow WindowModel wasn't included in the commit.

---
+++ Kerbal Space Program Mod/Kerbal_Space_Program_Mod.ProjectType/Deploy/WindowModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.ProjectSystem;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+
+namespace KSP4VS.Deploy
+{
+    internal class WindowModel
+    {
+        private readonly UnconfiguredProject project;
+
+        public WindowModel(UnconfiguredProject project)
+        {
+            this.project = project;
+        }
+
+        public string Name => Path.GetFileNameWithoutExtension(project.FullPath);
+
+        internal Task LoadFromProject()
+        {
+            return Task.CompletedTask;
+        }
+
+        internal void Save()
+        {
+
+        }
+    }
+}
